3d4159aa7b3488a9bbc94bff609487c6a259fde0
Ajaypal Gill <ajgill@ucdavis.edu>
2018-07-26 18:50:42 -0700

    Completed with basic implementation
    Used a struct and vectors to not be as reliant on user input for array sizes etc. Coded it all in main function and added a few extra if statements to check for errors. Next steps would be to seperate main into functions and make it more modular and clean up the output format etc.

--- SalesCommissions/main.cpp
+++ SalesCommissions/main.cpp
@@ -5,51 +5,74 @@
 //  Created by Aj Gill on 7/22/18.
 //  Copyright © 2018 Aj Gill. All rights reserved.
 //
-//You're a regional manager for an office beverage sales company, and right now you're in charge of paying your sales team they're monthly commissions.
-//
-//Sales people get paid using the following formula for the total commission: commission is 6.2% of profit, with no commission for any product to total less than zero.
-//
-//Input Description
-//You'll be given two matrices showing the sales figure per salesperson for each product they sold, and the expenses by product per salesperson. Example:
-//
-//Revenue
-//
-//         Frank   Jane
-//Tea       120    145
-//Coffee    243    265
-//
-//Expenses
-//
-//         Frank   Jane
-//Tea       130     59
-//Coffee    143    198
-
-//Output Description
-//Your program should calculate the commission for each salesperson for the month. Example:
-//
-//                Frank   Jane
-//Commission       6.20   9.49
-
-
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+struct Employee{
+    string name;
+    vector<int> revenue;
+    vector<int> expenses;
+    int profit;
+    int profitSum;
+    double commission;
+    
+};
 
 int main() {
     // Take in how how many members and products to create arrays of that size
     int numSales, numProducts;
     cout << "How many members on your sales team and how many products?" << endl;
     cin >> numSales >> numProducts;
-    int salesTeam[numProducts][numSales];
-    double commission[numSales];
-    // Initialize 2D array with for loops and names of members and products
-    cout << "Please enter the names of the sales team seperated by spaces" << endl;
-    for(int i=0; i<numSales; i++)
-        cin >> salesTeam[1][i];
-    cout << "Please enter the names of the products seperated by spaces" << endl;
-    for(int i=0; i<numProducts; i++)
-        cin >> salesTeam[i][0];
     
-
+    int revenue;
+    int expense;
+    string salesTeam[numSales];
+    vector <Employee> employee;
+    
+    for (int i=0; i<numSales; i++){
+        cout << "Input salesman #" << i+1 << "'s name" << endl;
+        Employee salesmani;
+        cin >> salesmani.name;
+        cout << "Input salesman #" << i+1 << "'s revenues"<< endl;
+        for (int j=0; j<numProducts; j++){
+            cin >> revenue;
+            if (revenue < 1){
+                cout << "You entered an invalid revenue" << endl;
+                break;
+            }
+            else{
+                salesmani.revenue.push_back(revenue);
+            }
+        }
+        
+        cout << "Input salesman #" << i+1 << "'s expenses"<< endl;
+        for (int k=0; k<numProducts; k++){
+            cin >> expense;
+            if (expense < 1){
+                cout << "You entered an invalid expense" << endl;
+                break;
+            }
+            else{
+                salesmani.expenses.push_back(expense);
+            }
+        }
+        employee.push_back(salesmani);
+    }
+    
+    for (int i=0; i<numSales; i++){
+        for (int j=0; j<numProducts; j++){
+            employee[i].profit = employee[i].revenue[j] - employee[i].expenses[j];
+            if (employee[i].profit>0)
+                employee[i].profitSum += employee[i].profit;
+        }
+        employee[i].commission = ((employee[i].profitSum)*(0.062));
+    }
+    
+    for (int i=0; i<numSales; i++){
+        cout << employee[i].name << "'s commission is " << employee[i].commission;
+    }
 }
